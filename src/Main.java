d9d5fcfb3fd7c230e1a4062162ef4610bba55537
e185712 <e185712@ie.u-ryukyu.ac.jp>
2018-11-26 12:33:42 +0900

    report4 step2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         String str = null;
-        str.length();
+
+        try {
+            str.length();
+        }
+        catch (NullPointerException e) {
+            System.out.println("例外が発生しました。");
+            System.out.println(e.getMessage());
+
+        }
     }
 }
